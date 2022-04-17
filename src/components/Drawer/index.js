c97989daf07dc4baaecea45d7b1d2331d2093b9b
VladislavBryl <54996000+VladislavBryl@users.noreply.github.com>
2022-04-17 17:45:10 +0300

    update 4

---
+++ src/components/Drawer/index.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import axios from 'axios';
+
+import Info from '../Info';
+import { useCart } from '../../hooks/useCart';
+
+import styles from './Drawer.module.scss';
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+function Drawer({ onClose, onRemove, items = [], opened }) {
+  const { cartItems, setCartItems, totalPrice } = useCart();
+  const [orderId, setOrderId] = React.useState(null);
+  const [isOrderComplete, setIsOrderComplete] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const onClickOrder = async () => {
+    try {
+      setIsLoading(true);
+      const { data } = await axios.post('/orders', {
+        items: cartItems,
+      });
+      setOrderId(data.id);
+      setIsOrderComplete(true);
+      setCartItems([]);
+
+      for (let i = 0; i < cartItems.length; i++) {
+        const item = cartItems[i];
+        await axios.delete('/cart/' + item.id);
+        await delay(1000);
+      }
+    } catch (error) {
+      alert('Помилка при створені замовлення :(');
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <div className={`${styles.overlay} ${opened ? styles.overlayVisible : ''}`}>
+      <div className={styles.drawer}>
+        <h2 className="d-flex justify-between mb-30">
+          Кошик <img onClick={onClose} className="cu-p" src="img/btn-remove.svg" alt="Close" />
+        </h2>
+
+        {items.length > 0 ? (
+          <div className="d-flex flex-column flex">
+            <div className="items flex">
+              {items.map((obj) => (
+                <div key={obj.id} className="cartItem d-flex align-center mb-20">
+                  <div
+                    style={{ backgroundImage: `url(${obj.imageUrl})` }}
+                    className="cartItemImg"></div>
+
+                  <div className="mr-20 flex">
+                    <p className="mb-5">{obj.title}</p>
+                    <b>{obj.price} руб.</b>
+                  </div>
+                  <img
+                    onClick={() => onRemove(obj.id)}
+                    className="removeBtn"
+                    src="img/btn-remove.svg"
+                    alt="Remove"
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="cartTotalBlock">
+              <ul>
+                <li>
+                  <span>До сплати:</span>
+                  <div></div>
+                  <b>{totalPrice} грн. </b>
+                </li>
+              </ul>
+              <button disabled={isLoading} onClick={onClickOrder} className="greenButton">
+                Оформити замовлення <img src="img/arrow.svg" alt="Arrow" />
+              </button>
+            </div>
+          </div>
+        ) : (
+          <Info
+            title={isOrderComplete ? 'Замовлення оформлено!' : 'Кошик пустий'}
+            description={
+              isOrderComplete
+                ? `Ваше замовлення #${orderId} у найближчий час буде передано кур'єру`
+                : 'Додайте хоча б одну пару кросівок для оформлення замовлення.'
+            }
+            image={isOrderComplete ? 'img/complete-order.jpg' : 'img/empty-cart.jpg'}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Drawer;
